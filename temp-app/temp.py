906b4c4a296e0a333c38fd19ae8bf64832e947e9
keprios <kyle.keprios@gmail.com>
2018-04-17 22:32:06 -0500

    Added self.unit_preference for defining C or F for preference

--- temp-app/temp.py
+++ temp-app/temp.py
@@ -15,6 +15,7 @@ class OneThermometer:
             print('Device file not found!')
             raise AssertionError
         
+        self.unit_preference = None # 'C' (Celcius) or 'F' (Farinheight)
         self.current_temp = None
         self.cieling_temp = None
         self.floor_temp = None
@@ -29,25 +30,31 @@ class OneThermometer:
     def hexToDec(hex_temp):
         dec = 0
         
+        # Convert negative value if leading byte is 'FF'
         if hex_temp[:2] == 'FF':
             hex_temp = bin(int(hex_temp, 16))[2:]
+            # Flip bits
             for bit in hex_temp:
                 if bit == '1':
                     temp += '0'
                 elif bit == '0':
                     temp += '1'
             temp = bin(int(temp, 2))[:2]
+            # Add 1 to binary string, convert to dec, make negative (int)
             dec = -int(bin(int(temp,2) + int('1',2))[2:], 2)
         else:
+            # Positive temp from hex to dec (int)
             dec = int(temp, 16)
 
+        # return (int)
         return dec
+
 
     # Read the RAW t=##### number
     # Include the '-' if value is negative. Ex: -#####
     # Variables:
     #   - self (object): of OneThermometer
-    #   - forc (string): Specifies return of Farenheight ("F) or Celcius ("C")
+    #   - forc (string): Specifies return of Farinheight ("F) or Celcius ("C")
     def readRAW (self, forc):
         temp_file = ''
         hex_line = ''
@@ -72,6 +79,11 @@ class OneThermometer:
             print("Readings do not match")
             return -254
 
-        # Convert hex to decimal
-        temp = 
-        self.current_temp =  int(str(findall("=-*\d*$", temp_file.readline()))
+        # Convert hex to decimal and divide by 1000 for float (##### -> ##.###)
+        # Default unit = Celcius
+        temp = hexToDec(hex_line) / 1000
+        
+        if forc.upper() == 'F':
+            
+
+        
